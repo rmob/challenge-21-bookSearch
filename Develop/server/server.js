52ea29ec7d725ef1127098f5405431b4ec0fd9c8
rmob <riley.obryan@gmail.com>
2023-04-03 19:17:51 -0700

    changed boilerplate on server.js file to refactor for apollo/graphql

---
+++ Develop/server/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
